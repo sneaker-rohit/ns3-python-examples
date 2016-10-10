d024c2f662a5f31e9609bdf3d1018f9ac20503c8
rohittahiliani <rohit.tahil@gmail.com>
2016-10-11 01:59:54 +0530

    fixed the errors and comments

--- wireless/wifi-tcp.py
+++ wireless/wifi-tcp.py
@@ -1,36 +1,36 @@
-""" Mode: Python;
-  Copyright (c) 2016
- 
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License version 2 as
-  published by the Free Software Foundation;
- 
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
- 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- 
-  Ported to Python By: Rohit P. Tahiliani <rohit.tahil@gmail.com>
-                        Mohit P. Tahiliani <tahiliani@mnitk.edu.in>
-.
-  This is a simple example to test TCP over 802.11n (with MPDU aggregation enabled).
- 
-  Network topology:
- 
-    Ap    STA
-    *      *
-    |      |
-    n1     n2
- 
- In this example, an HT station sends TCP packets to the access point. 
- We report the total throughput received during a window of 100ms. 
- The user can specify the application data rate and choose the variant
- of TCP i.e. congestion control algorithm to use.
- """
+# -*-  Mode: Python; -*-
+# /*
+#  * Copyright (c) 2016 
+#  *
+#  * This program is free software; you can redistribute it and/or modify
+#  * it under the terms of the GNU General Public License version 2 as
+#  * published by the Free Software Foundation;
+#  *
+#  * This program is distributed in the hope that it will be useful,
+#  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  * GNU General Public License for more details.
+#  *
+#  * You should have received a copy of the GNU General Public License
+#  * along with this program; if not, write to the Free Software
+#  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#  *
+#  * Ported to Python by: Rohit P. Tahiliani <rohit.tahil@gmail.com>  
+#  *
+#  * This is a simple example to test TCP over 802.11n (with MPDU aggregation enabled).
+#  *
+#  * Network topology:
+#  *
+#  *   Ap    STA
+#  *   *      *
+#  *   |      |
+#  *   n1     n2
+#  *
+#  * In this example, an HT station sends TCP packets to the access point. 
+#  * We report the total throughput received during a window of 100ms. 
+#  * The user can specify the application data rate and choose the variant
+#  * of TCP i.e. congestion control algorithm to use.                 
+#  */
 
 import ns.applications
 import ns.core
@@ -41,24 +41,22 @@ import ns.point_to_point
 import ns.wifi
 
 
-sink = ns.network.PacketSink ()                          
+sink = ns.applications.PacketSink ()                          
 #The value of the last total received bytes
 lastTotalRx = 0              
 
-def CalculateThroughput():
+def CalculateThroughput ():
   #Return the simulator's virtual time. 
-  now = ns.core.Simulator.Now()
+  now = ns.core.Simulator.Now ()
   #Convert Application RX Packets to MBits.
-  cur = (sink.GetTotalRx() - lastTotalRx) * (8/1e5)
-  print now.GetSeconds() + "s: \t" + cur + "Mbit/s"
+  cur = (sink.GetTotalRx () - lastTotalRx) * (8/1e5)
+  print now.GetSeconds () + "s: \t" + cur + "Mbit/s"
   lastTotalRx = sink.GetTotalRx ()
   ns.core.Simulator.Schedule (ns.core.MilliSeconds (100), CalculateThroughput)
 
-
-
 def main(argv):
   #Command line argument parser setup.
-  cmd = ns.core.CommandLine()
+  cmd = ns.core.CommandLine ()
   #Transport layer payload size in bytes. 
   cmd.payloadSize = 1472                       
   #Application layer datarate.
@@ -72,12 +70,12 @@ def main(argv):
   #PCAP Tracing is enabled or not.
   cmd.pcapTracing = "False"                          
 
-  cmd.AddValue ("payloadSize", "Payload size in bytes", cmd.payloadSize)
-  cmd.AddValue ("dataRate", "Application data ate", cmd.dataRate)
-  cmd.AddValue ("tcpVariant", "Transport protocol to use: TcpTahoe, TcpReno, TcpNewReno, TcpWestwood, TcpWestwoodPlus ", cmd.tcpVariant);
-  cmd.AddValue ("phyRate", "Physical layer bitrate", cmd.phyRate)
-  cmd.AddValue ("simulationTime", "Simulation time in seconds", cmd.simulationTime)
-  cmd.AddValue ("pcap", "Enable/disable PCAP Tracing", cmd.pcapTracing)
+  cmd.AddValue ("payloadSize", "Payload size in bytes")
+  cmd.AddValue ("dataRate", "Application data ate")
+  cmd.AddValue ("tcpVariant", "Transport protocol to use: TcpTahoe, TcpReno, TcpNewReno, TcpWestwood, TcpWestwoodPlus ")
+  cmd.AddValue ("phyRate", "Physical layer bitrate")
+  cmd.AddValue ("simulationTime", "Simulation time in seconds")
+  cmd.AddValue ("pcap", "Enable/disable PCAP Tracing")
   cmd.Parse (sys.argv)
 
   #No fragmentation and no RTS/CTS 
@@ -87,17 +85,17 @@ def main(argv):
   #Configure TCP Options 
   ns.core.Config.SetDefault ("ns3::TcpSocket::SegmentSize", ns.core.UintegerValue (cmd.payloadSize))
 
-  WifiMacHelper wifiMac;
-  WifiHelper wifiHelper;
+  wifiMac = ns.wifi.WifiMacHelper ()
+  wifiHelper = ns.wifi.WifiHelper ()
   wifiHelper.SetStandard (ns.wifi.WIFI_PHY_STANDARD_80211n_5GHZ)
 
   #Set up Legacy Channel 
-  wifiChannel = ns.core.YansWifiChannelHelper ()
+  wifiChannel = ns.wifi.YansWifiChannelHelper ()
   wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel")
   wifiChannel.AddPropagationLoss ("ns3::FriisPropagationLossModel", "Frequency", ns.core.DoubleValue (5e9))
 
   #Setup Physical Layer 
-  wifiPhy = ns.core.YansWifiPhyHelper.Default ()
+  wifiPhy = ns.wifi.YansWifiPhyHelper.Default ()
   wifiPhy.SetChannel (wifiChannel.Create ())
   wifiPhy.Set ("TxPowerStart", ns.core.DoubleValue (10.0))
   wifiPhy.Set ("TxPowerEnd", ns.core.DoubleValue (10.0))
@@ -114,61 +112,61 @@ def main(argv):
 
   networkNodes = ns.network.NodeContainer ()
   networkNodes.Create (2)
-  apWifiNode = Node(networkNodes.Get (0))
-  staWifiNode = Node(networkNodes.Get (1))
+  apWifiNode  = networkNodes.Get (0)
+  staWifiNode = networkNodes.Get (1)
 
   #Configure AP 
-  ssid = ns.wifi.Ssid("network")
+  ssid = ns.wifi.Ssid ("network")
   wifiMac.SetType ("ns3::ApWifiMac",
-                    "Ssid", SsidValue (ssid));
+                    "Ssid", ns.wifi.SsidValue (ssid))
 
   apDevice = ns.network.NodeContainer ()
-  apDevice = wifiHelper.Install (wifiPhy, wifiMac, apWifiNode);
+  apDevice = wifiHelper.Install (wifiPhy, wifiMac, apWifiNode)
 
   #Configure STA 
   wifiMac.SetType ("ns3::StaWifiMac",
-                    "Ssid", SsidValue (ssid));
+                    "Ssid", ns.wifi.SsidValue (ssid))
 
   staDevices = ns.network.NetDeviceContainer ()
-  staDevices = wifiHelper.Install (wifiPhy, wifiMac, staWifiNode);
+  staDevices = wifiHelper.Install (wifiPhy, wifiMac, staWifiNode)
 
   #Mobility model 
-  mobility = ns.mobility.MobilityHelper()
-  positionAlloc = ns.mobility.ListPositionAllocator()
-  positionAlloc.Add(ns.core.Vector3D(0.0, 0.0, 0.0))
-  positionAlloc.Add(ns.core.Vector3D(1.0, 1.0, 1.0))
+  mobility = ns.mobility.MobilityHelper ()
+  positionAlloc = ns.mobility.ListPositionAllocator ()
+  positionAlloc.Add (ns.core.Vector3D (0.0, 0.0, 0.0))
+  positionAlloc.Add (ns.core.Vector3D (1.0, 1.0, 1.0))
 
   mobility.SetPositionAllocator (positionAlloc);
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.Install (apWifiNode);
-  mobility.Install (staWifiNode);
+  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel")
+  mobility.Install (apWifiNode)
+  mobility.Install (staWifiNode)
 
   #Internet stack 
   stack = ns.internet.InternetStackHelper ()
   stack.Install (networkNodes);
 
   address = ns.internet.Ipv4AddressHelper ()
-  address.SetBase ("10.0.0.0", "255.255.255.0");
+  address.SetBase (ns.network.Ipv4Address ("10.0.0.0"), ns.network.Ipv4Mask ("255.255.255.0"))
   apInterface = ns.internet.Ipv4InterfaceContainer ()
-  apInterface = address.Assign (apDevice);
+  apInterface = address.Assign (apDevice)
   staInterface = ns.internet.Ipv4InterfaceContainer ()
-  staInterface = address.Assign (staDevices);
+  staInterface = address.Assign (staDevices)
 
   #Populate routing table 
   ns.internet.Ipv4GlobalRoutingHelper.PopulateRoutingTables ()
 
   #Install TCP Receiver on the access point 
-  sinkHelper = PacketSinkHelper ("ns3::TcpSocketFactory", ns.internet.InetSocketAddress (ns.internet.Ipv4Address.GetAny (), 9))
-  sinkApp = ApplicationContainer (sinkHelper.Install (apWifiNode))
-  sink = ns.network.PacketSink (sinkApp.Get (0))
+  sinkHelper = ns.applications.PacketSinkHelper ("ns3::TcpSocketFactory", ns.network.InetSocketAddress (ns.network.Ipv4Address.GetAny (), 9))
+  sinkApp = ns.network.ApplicationContainer (sinkHelper.Install (apWifiNode))
+  sink = ns.applications.PacketSink (sinkApp.Get (0))
 
   #Install TCP/UDP Transmitter on the station 
-  server = OnOffHelper("ns3::TcpSocketFactory", (ns.internet.InetSocketAddress (apInterface.GetAddress (0), 9)))
-  server.SetAttribute ("PacketSize", ns.core.UintegerValue (payloadSize))
+  server = ns.applications.OnOffHelper ("ns3::TcpSocketFactory", (ns.network.InetSocketAddress (apInterface.GetAddress (0), 9)))
+  server.SetAttribute ("PacketSize", ns.core.UintegerValue (cmd.payloadSize))
   server.SetAttribute ("OnTime", ns.core.StringValue ("ns3::ConstantRandomVariable[Constant=1]"))
   server.SetAttribute ("OffTime", ns.core.StringValue ("ns3::ConstantRandomVariable[Constant=0]"))
-  server.SetAttribute ("DataRate", ns.network.DataRateValue (ns.network.DataRate (dataRate)))
-  serverApp = ApplicationContainer(server.Install (staWifiNode))
+  server.SetAttribute ("DataRate", ns.network.DataRateValue (ns.network.DataRate (cmd.dataRate)))
+  serverApp = ns.network.ApplicationContainer (server.Install (staWifiNode))
 
   #Start Applications 
   sinkApp.Start (ns.core.Seconds (0.0))
@@ -176,18 +174,18 @@ def main(argv):
   ns.core.Simulator.Schedule (ns.core.Seconds (1.1), CalculateThroughput)
 
   #Enable Traces 
-  if pcapTracing:
-      wifiPhy.SetPcapDataLinkType (ns.core.YansWifiPhyHelper.DLT_IEEE802_11_RADIO)
+  if cmd.pcapTracing:
+      wifiPhy.SetPcapDataLinkType (ns.wifi.YansWifiPhyHelper.DLT_IEEE802_11_RADIO)
       wifiPhy.EnablePcap ("AccessPoint", apDevice)
       wifiPhy.EnablePcap ("Station", staDevices)
     
 
   #Start Simulation 
-  ns.core.Simulator.Stop (ns.core.Seconds (simulationTime + 1))
+  ns.core.Simulator.Stop (ns.core.Seconds (cmd.simulationTime + 1))
   ns.core.Simulator.Run ()
   ns.core.Simulator.Destroy ()
 
-  averageThroughput = ((sink.GetTotalRx() * 8) / (1e6  * simulationTime))
+  averageThroughput = ((sink.GetTotalRx () * 8) / (1e6  * cmd.simulationTime))
   if averageThroughput < 50:  
       ns.core.NS_LOG_ERROR ("Obtained throughput is not in the expected boundaries!")
       exit (1)
@@ -196,4 +194,4 @@ def main(argv):
 
 if __name__ == '__main__':
   import sys 
-  sys.exit(main(sys.argv))
+  sys.exit (main(sys.argv))
