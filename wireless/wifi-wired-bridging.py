190e48ac5d9a1b5b89c40058f9320f6fa1a508ac
rohittahiliani <rohit.tahil@gmail.com>
2016-10-22 22:16:45 +0530

    fixed errors with cmd line args

--- wireless/wifi-wired-bridging.py
+++ wireless/wifi-wired-bridging.py
@@ -54,36 +54,41 @@ import ns.network
 import ns.csma
 import ns.internet
 import ns.bridge
+import ns.visualizer
 
 def main (argv):
-  nWifis = 2
-  nStas = 2
-  sendIp = True
-  writeMobility = False
-
   cmd = ns.core.CommandLine ()
+  cmd.nWifis = 2
+  cmd.nStas = 2
+  cmd.sendIp = True
+  cmd.writeMobility = False
   cmd.AddValue ("nWifis", "Number of wifi networks")
   cmd.AddValue ("nStas", "Number of stations per wifi network")
   cmd.AddValue ("SendIp", "Send Ipv4 or raw packets")
   cmd.AddValue ("writeMobility", "Write mobility trace")
   cmd.Parse (sys.argv)
 
+  nWifis = cmd.nWifis
+  nStas = cmd.nStas
+  sendIp = cmd.sendIp
+  writeMobility = cmd.writeMobility
+
   backboneNodes = ns.network.NodeContainer ()
   backboneDevices = ns.network.NetDeviceContainer ()
-  backboneInterfaces = ns.network.Ipv4InterfaceContainer ()
+  backboneInterfaces = ns.internet.Ipv4InterfaceContainer ()
   
   staNodes = ns.network.NodeContainer ()
   staDevices = ns.network.NetDeviceContainer ()
   apDevices = ns.network.NetDeviceContainer ()
-  staInterfaces = ns.network.Ipv4InterfaceContainer ()
-  apInterfaces = ns.network.Ipv4InterfaceContainer ()
+  staInterfaces = ns.internet.Ipv4InterfaceContainer ()
+  apInterfaces = ns.internet.Ipv4InterfaceContainer ()
 
   stack = ns.internet.InternetStackHelper ()
-  csma = ns.internet.CsmaHelper ()
+  csma = ns.csma.CsmaHelper ()
   ip = ns.internet.Ipv4AddressHelper ()
   ip.SetBase (ns.network.Ipv4Address ("192.168.0.0"), ns.network.Ipv4Mask ("255.255.255.0"))
 
-  backboneNodes.Create (nWifis)
+  backboneNodes.Create (cmd.nWifis)
   stack.Install (backboneNodes)
 
   backboneDevices = csma.Install (backboneNodes)
@@ -93,7 +98,7 @@ def main (argv):
   wifiPhy = ns.wifi.YansWifiPhyHelper.Default ()
   wifiPhy.SetPcapDataLinkType (ns.wifi.YansWifiPhyHelper.DLT_IEEE802_11_RADIO) 
 
-  for i in range (0,nWifis):
+  for i in range (0,cmd.nWifis):
       #calculate ssid for wifi subnetwork
       ssid = ns.wifi.Ssid ("wifi-default-" + str (i))
 
@@ -109,7 +114,7 @@ def main (argv):
       wifiChannel = ns.wifi.YansWifiChannelHelper.Default ()
       wifiPhy.SetChannel (wifiChannel.Create ())
 
-      sta.Create (nStas)
+      sta.Create (cmd.nStas)
       mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
                                      "MinX", ns.core.DoubleValue (wifiX),
                                      "MinY", ns.core.DoubleValue (0.0),
@@ -137,7 +142,7 @@ def main (argv):
                                  "Mode", ns.core.StringValue ("Time"),
                                  "Time", ns.core.StringValue ("2s"),
                                  "Speed", ns.core.StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"),
-                                 "Bounds", ns.core.RectangleValue (ns.core.Rectangle (wifiX, wifiX+5.0,0.0, (nStas+1)*5.0)))
+                                 "Bounds", ns.core.RectangleValue (ns.core.Rectangle (wifiX, wifiX+5.0,0.0, (cmd.nStas+1)*5.0)))
       mobility.Install (sta);
       wifiMac.SetType ("ns3::StaWifiMac",
                        "Ssid", ns.wifi.SsidValue (ssid))
@@ -154,30 +159,34 @@ def main (argv):
       wifiX = wifiX + 20.0
 
   dest = ns.network.Address ()
-  if sendIp:
+  if cmd.sendIp:
       dest = ns.network.InetSocketAddress (staInterfaces(1).GetAddress (1), 1025)
       protocol = "ns3::UdpSocketFactory"
   else:
       tmp = ns.network.PacketSocketAddress ()
-      tmp.SetSingleDevice (staDevices(0).Get (0).GetIfIndex ())
-      tmp.SetPhysicalAddress (staDevices(1).Get (0).GetAddress ())
+      tmp.SetSingleDevice (staDevices[0].Get (0).GetIfIndex ())
+      tmp.SetPhysicalAddress (staDevices[1].Get (0).GetAddress ())
       tmp.SetProtocol (0x807)
       dest = tmp
       protocol = "ns3::PacketSocketFactory"
 
   onoff = ns.applications.OnOffHelper (protocol, dest)
-  onoff.SetConstantRate (ns.core.DataRate ("500kb/s"))
-  apps = ns.applications.ApplicationContainer (onoff.Install (staNodes(0).Get (0)))
+  onoff.SetConstantRate (ns.core.DataRateValue (ns.core.DataRate ("500kb/s")))
+  apps = ns.applications.ApplicationContainer (onoff.Install (staNodes[0].Get (0)))
   apps.Start (ns.core.Seconds (0.5))
   apps.Stop (ns.core.Seconds (3.0))
 
-  wifiPhy.EnablePcap ("wifi-wired-bridging", apDevices(0))
-  wifiPhy.EnablePcap ("wifi-wired-bridging", apDevices(1))
+  wifiPhy.EnablePcap ("wifi-wired-bridging", apDevices[0])
+  wifiPhy.EnablePcap ("wifi-wired-bridging", apDevices[1])
 
-  if writeMobility:
-      ascii = ns.core.AsciiTraceHelper ()
+  if cmd.writeMobility:
+      ascii = ns.network.AsciiTraceHelper ()
       ns.mobility.MobilityHelper.EnableAsciiAll (ascii.CreateFileStream ("wifi-wired-bridging.mob"))
   
   ns.core.Simulator.Stop (ns.core.Seconds (5.0))
   ns.core.Simulator.Run ()
   ns.core.Simulator.Destroy ()
+
+if __name__ == '__main__':
+  import sys
+  main(sys.argv)
